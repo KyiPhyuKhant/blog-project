826b48997edc0073a7c1357576d61b3aabc37cd7
KyiPhyu <kyiphyu@eramyanmar.com>
2020-05-07 13:08:41 +0630

    create new project and add some route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+<script src="/js/app.js"></script>
+</body>
+</html>
